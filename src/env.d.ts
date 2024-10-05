72622a3ef07a0c95aa4f5f553b1c82bc409ffc8e
VictorRodas99 <victormatiasrodas100@gmail.com>
2024-10-05 11:14:14 -0400

    update types for env variables

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,13 @@
 /// <reference path="../.astro/types.d.ts" />
+
+interface ImportMetaEnv {
+  readonly SUPABASE_URL: string
+  readonly SUPABASE_ANON_KEY: string
+  readonly CLOUDINARY_API_KEY_SECRET: string
+  readonly CLOUDINARY_CLOUD_NAME: string
+  readonly CLOUDINARY_API_KEY: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}

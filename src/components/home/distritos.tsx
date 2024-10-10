93a9fe9e47ed2e498aedc3898ed39264b26575b1
VictorRodas99 <victormatiasrodas100@gmail.com>
2024-10-10 14:51:19 -0300

    add button to get more districts + add animations and styles

---
+++ src/components/home/distritos.tsx
@@ -0,0 +1,77 @@
+import type { Distrito } from '../../types'
+import SeeMoreDistritosButton from './see-more-distritos'
+import ArrowRight from '../icons/arrow-right'
+import { useState } from 'react'
+import { motion } from 'framer-motion'
+import BackgroundLightOne from '../lights/background-light-one'
+
+export default function Distritos({
+  initialDistritos
+}: {
+  initialDistritos: Distrito[] | null
+}) {
+  const [distritos, setDistritos] = useState(initialDistritos ?? [])
+
+  const updateDistritos = (newDistritos: Distrito[]) => {
+    // TODO: validate with zod
+    setDistritos((prev) => [...prev, ...newDistritos])
+  }
+
+  return (
+    <section className="relative flex flex-col gap-12 justify-center items-center w-full">
+      <div className="absolute w-full h-fit bottom-[-5%] -right-12 rounded-full opacity-30  ">
+        <BackgroundLightOne />
+      </div>
+
+      {distritos.length === 0 ? (
+        <h2>Sin Distritos a mostrar</h2>
+      ) : (
+        <>
+          <h2 className="text-3xl font-bold font-outift">Distritos</h2>
+          <motion.div className="grid grid-cols-3 gap-5" layout>
+            {distritos?.map((distrito, index) => (
+              <motion.div
+                key={distrito.id}
+                className="bg-white shadow-lg border rounded-lg max-w-[300px] overflow-hidden"
+                initial={{ opacity: 0, y: 50 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{
+                  duration: 0.5,
+                  delay: index * 0.1,
+                  ease: 'easeOut'
+                }}
+                layout
+              >
+                <header className="relative w-[300px] h-[270px] overflow-hidden rounded-t-lg">
+                  <img
+                    className="absolute inset-0 w-full h-full object-cover transition-transform duration-300 hover:scale-105 rounded-t-lg"
+                    src={distrito.main_image ?? ''}
+                    alt={distrito.name}
+                    width={300}
+                    height={270}
+                  />
+                </header>
+                <div className="p-4 flex flex-col gap-5">
+                  <div className="flex flex-col gap-5">
+                    <h5 className="font-bold">{distrito.name}</h5>
+                    <p className="text-sm h-20">{distrito.description}</p>
+                  </div>
+                  <div className="w-full">
+                    <a
+                      className="flex gap-2 items-center w-fit hover:text-primary transition-colors"
+                      href={`/distrito/${distrito.id}`}
+                    >
+                      Visitar
+                      <ArrowRight size={15} />
+                    </a>
+                  </div>
+                </div>
+              </motion.div>
+            ))}
+          </motion.div>
+        </>
+      )}
+      <SeeMoreDistritosButton updateDistritos={updateDistritos} />
+    </section>
+  )
+}
---
+++ src/components/home/see-more-distritos.tsx
@@ -0,0 +1,92 @@
+import { useRef, useState } from 'react'
+import { API_STATES, DEFAULT_API_LIMIT_FOR } from '../../pages/api/_states'
+import type { Distrito } from '../../types'
+import { motion } from 'framer-motion'
+import Loader from '../icons/loader'
+import { truncateText } from '../../utils/formatter'
+
+async function fetchMoreDistritos({ page }: { page: number }) {
+  if (typeof page !== 'number' || Number.isNaN(page)) {
+    throw new Error('Page param must be of type number')
+  }
+
+  const response = await fetch(`/api/distritos?page=${page}`)
+  const jsonApiResponse = await response.json()
+
+  if (jsonApiResponse.data.statusText !== API_STATES.success) {
+    // TODO: add toast for not found cases
+    return null
+  }
+
+  const distritos = jsonApiResponse.data.data as Distrito[]
+
+  return distritos.map((distrito) => ({
+    ...distrito,
+    description: truncateText(distrito.description, { maxWords: 15 })
+  }))
+}
+
+interface SeeMoreButtonProps {
+  updateDistritos: (newDistritos: Distrito[]) => void
+}
+
+export default function SeeMoreDistritosButton({
+  updateDistritos
+}: SeeMoreButtonProps) {
+  const currentPage = useRef(1)
+
+  const [isLoading, setIsLoading] = useState(false)
+  const [isNullResponse, setIsNullResponse] = useState(false)
+
+  const getMoreDistritos = async () => {
+    currentPage.current++
+
+    setIsLoading(true)
+    const distritos = await fetchMoreDistritos({ page: currentPage.current })
+    setIsLoading(false)
+
+    if (!distritos || distritos?.length === 0) {
+      return
+    }
+
+    if (distritos.length < DEFAULT_API_LIMIT_FOR.distritos) {
+      setIsNullResponse(true)
+    }
+
+    updateDistritos(distritos)
+  }
+
+  return isNullResponse ? null : (
+    <button
+      type="button"
+      className="relative text-sm h-10 bg-primary border border-primary rounded-md text-white hover:bg-primary/90 p-2 transition-colors"
+      disabled={isLoading}
+      onClick={getMoreDistritos}
+    >
+      <motion.div
+        initial={false}
+        animate={{
+          opacity: isLoading ? 0 : 1,
+          y: isLoading ? -30 : 0
+        }}
+        transition={{ duration: 0.2 }}
+      >
+        Ver más distritos
+      </motion.div>
+      <motion.div
+        initial={false}
+        animate={{
+          opacity: isLoading ? 1 : 0,
+          scale: isLoading ? 1 : 0
+        }}
+        transition={{ duration: 0.3 }}
+        className="absolute inset-0 flex items-center justify-center"
+      >
+        <Loader className="w-6 h-6 animate-spin" />
+      </motion.div>
+      <span className="sr-only">
+        {isLoading ? 'Loading' : 'Ver más distritos'}
+      </span>
+    </button>
+  )
+}

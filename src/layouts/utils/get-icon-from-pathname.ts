7a1740a3bdf5147aea96373ca3ca4346ee55a3a6
VictorRodas99 <victormatiasrodas100@gmail.com>
2024-10-10 16:39:50 -0300

    add utility to get specific icon for nav item

---
+++ src/layouts/utils/get-icon-from-pathname.ts
@@ -0,0 +1,32 @@
+import { House, Bird, Crown } from 'lucide-react'
+import type { getWebsitePaths } from '../../utils/general'
+
+type PathsWithoutIcons = ReturnType<typeof getWebsitePaths>
+type Keys = keyof PathsWithoutIcons
+export type PathsWithoutIconsAsEntries = [Keys, PathsWithoutIcons[Keys]][]
+
+export default function getIconFromPathName(
+  name: Keys,
+  pathsWithoutIcons: PathsWithoutIcons
+) {
+  if (!Object.keys(pathsWithoutIcons).includes(name)) {
+    throw new Error('Given param does not exists as a valid path name')
+  }
+
+  switch (name) {
+    case 'home':
+      return House
+
+    case 'places':
+      return Bird
+
+    case 'preferences':
+      return Crown
+
+    case 'aboout':
+      return null
+
+    default:
+      return null
+  }
+}

e6e4f7f249b288a40bd33e5e40fb0141427f865c
VictorRodas99 <victormatiasrodas100@gmail.com>
2024-10-13 00:35:33 -0300

    add new api endpoint to get districts by id

---
+++ src/pages/api/distritos/[id].ts
@@ -0,0 +1,45 @@
+import type { APIRoute } from 'astro'
+import {
+  createErrorResponse,
+  createSucessResponse
+} from '../_utils/create-response'
+import { supabase } from '@/lib/supabase'
+
+export const GET: APIRoute = async ({ params }) => {
+  const { id } = params
+
+  if (!id) {
+    return createErrorResponse({
+      errorMessage: 'Missing id param to get district',
+      responseStatus: 403
+    })
+  }
+
+  const parsedId = Number(id)
+
+  if (Number.isNaN(parsedId) || parsedId <= 0) {
+    return createErrorResponse({
+      errorMessage: 'Id param must be a positive number!',
+      responseStatus: 403
+    })
+  }
+
+  const { data: distrito, error } = await supabase
+    .from('distrito')
+    .select('id, name, description, main_image')
+    .eq('id', parsedId)
+
+  if (error) {
+    console.error(error)
+
+    return createErrorResponse({
+      errorMessage: error.message,
+      responseStatus: 505
+    })
+  }
+
+  return createSucessResponse({
+    responseStatus: 202,
+    data: distrito.length === 0 ? null : distrito
+  })
+}

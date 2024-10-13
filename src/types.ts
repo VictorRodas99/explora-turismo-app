64c3a2103caaff5c95d70f65e3097b566f9c5a1f
VictorRodas99 <victormatiasrodas100@gmail.com>
2024-10-13 00:33:04 -0300

    update types for place categories

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,15 @@
 import type { Database } from './lib/database'
+import type { interestPointCategories } from './constants'
 
 export type Distrito = Database['public']['Tables']['distrito']['Row']
+
+export type InterestPointCategoryEnglish = keyof typeof interestPointCategories
+export type InterestPointCategory =
+  (typeof interestPointCategories)[InterestPointCategoryEnglish]
+
+export type InterestPointDB =
+  Database['public']['Tables']['puntos_de_interes']['Row']
+
+export interface InterestPoint extends InterestPointDB {
+  tipo: InterestPointCategory
+}

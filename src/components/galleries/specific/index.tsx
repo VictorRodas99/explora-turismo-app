ca09ebc78f7f49568921f230507bca4bd8322708
VictorRodas99 <victormatiasrodas100@gmail.com>
2024-10-17 23:59:53 -0300

    implement functionalities for gallery

--- src/components/galleries/specific/index.tsx
+++ src/components/galleries/specific/index.tsx
@@ -1,7 +1,9 @@
 import { cn } from '@/utils/cn'
 import type { getImagesFromResponse } from '@/utils/formatter'
 import { Logs } from 'lucide-react'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import ImageTour from './image-tour'
+import { AnimatePresence } from 'framer-motion'
 
 export default function Gallery({
   images,
@@ -10,6 +12,8 @@ export default function Gallery({
   images: ReturnType<typeof getImagesFromResponse>
   retrieveError?: string
 }) {
+  const [isOpenImageTour, setisOpenImageTour] = useState(false)
+
   useEffect(() => {
     if (retrieveError === 'timeout') {
       console.warn(
@@ -23,30 +27,66 @@ export default function Gallery({
     return <h2>Sin imágenes</h2>
   }
 
+  // biome-ignore lint/correctness/useExhaustiveDependencies(isOpenImageTour): We only need to update is isOpenImageTour changes
+  useEffect(() => {
+    const classListMethod = isOpenImageTour
+      ? ('add' as const)
+      : ('remove' as const)
+
+    document.body.classList[classListMethod]('overflow-hidden')
+  }, [isOpenImageTour])
+
+  useEffect(() => {
+    const ESCAPE_KEY_NAME = 'Escape'
+
+    window.addEventListener('keyup', (event) => {
+      const { key } = event
+
+      if (isOpenImageTour && key === ESCAPE_KEY_NAME) {
+        setisOpenImageTour(false)
+      }
+    })
+  }, [isOpenImageTour])
+
   return (
-    <section className="relative grid grid-cols-4 gap-2 rounded-xl overflow-hidden">
-      {images.slice(0, 5).map(({ assetId, url, name }, index) => (
+    <>
+      <section className="relative grid grid-cols-4 gap-2 rounded-xl overflow-hidden">
+        {images.slice(0, 5).map(({ assetId, url, name }, index) => (
+          <button
+            id={assetId}
+            type="button"
+            key={assetId}
+            className={cn(
+              'max-h-[180px] hover:brightness-75 transition-all focus:outline-none',
+              {
+                'col-span-2 row-span-2 max-h-max': index === 0
+              }
+            )}
+            onClick={() => setisOpenImageTour(true)}
+          >
+            <img
+              src={url}
+              alt={`imagen ${name}`}
+              className="w-full h-full object-cover"
+            />
+          </button>
+        ))}
         <button
           type="button"
-          key={assetId}
-          className={cn('max-h-[180px] hover:brightness-75 transition-all', {
-            'col-span-2 row-span-2 max-h-max': index === 0
-          })}
+          onClick={() => setisOpenImageTour(true)}
+          className="absolute flex gap-2 items-center right-5 bottom-5 bg-primary text-white text-sm px-3 py-2 rounded-lg hover:bg-white hover:text-primary transition-colors"
         >
-          <img
-            src={url}
-            alt={`imagen ${name}`}
-            className="w-full h-full object-cover"
-          />
+          <Logs size={18} />
         </button>
-      ))}
-      <button
-        type="button"
-        className="absolute flex gap-2 items-center right-5 bottom-5 border border-primary bg-white text-sm px-3 py-2 rounded-lg"
-      >
-        <Logs size={15} />
-        Mostrar todas las fotos
-      </button>
-    </section>
+      </section>
+      <AnimatePresence>
+        {isOpenImageTour && (
+          <ImageTour
+            images={images}
+            closeCallback={() => setisOpenImageTour(false)}
+          />
+        )}
+      </AnimatePresence>
+    </>
   )
 }

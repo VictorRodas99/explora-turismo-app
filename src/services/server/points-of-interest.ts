8b47b4944defc04037271d2d06821d83585559b8
VictorRodas99 <victormatiasrodas100@gmail.com>
2024-10-22 15:47:25 -0300

    add new method to get contacts from place

--- src/services/server/points-of-interest.ts
+++ src/services/server/points-of-interest.ts
@@ -1,7 +1,9 @@
 import type { APIContext } from 'astro'
 import { GET as APIgetPointOfInterestByDistrict } from '@/pages/api/puntos/distrito/[id]'
+import { GET as APIgetPointOfInterestById } from '@/pages/api/puntos/[id]'
+import { GET as APIgetPointOfInterestContactById } from '@/pages/api/puntos/[id]/contactos'
 import { API_STATES } from '@/pages/api/_states'
-import type { InterestPoint } from '@/types'
+import type { InterestPoint, InterestPointContacts } from '@/types'
 
 export async function getPointOfInterestByDistrict(context: APIContext) {
   const response = await APIgetPointOfInterestByDistrict(context)
@@ -14,4 +16,35 @@ export async function getPointOfInterestByDistrict(context: APIContext) {
   }
 
   return data as InterestPoint[]
+}
+
+export async function getPoinstOfInterestById(context: APIContext) {
+  const response = await APIgetPointOfInterestById(context)
+  const json = await response.json()
+
+  const { statusText, data } = json.data
+
+  if (statusText === API_STATES.error || !data) {
+    return null
+  }
+
+  return data as InterestPoint[]
+}
+
+export async function getPointOfInterestContacts(context: APIContext) {
+  const response = await APIgetPointOfInterestContactById(context)
+  const json = await response.json()
+
+  const { statusText } = json.data
+  const data: InterestPointContacts = json.data.data
+
+  if (statusText === API_STATES.error || !data) {
+    return null
+  }
+
+  return {
+    id: data.id,
+    instagramUser: data.instagram_user,
+    phoneNumbers: data.phone_number
+  }
 }

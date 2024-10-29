eebc6c94daf9fe521a749eb42107c94e2f366a3d
VictorRodas99 <victormatiasrodas100@gmail.com>
2024-10-29 10:52:22 -0300

    add new funcionality to gallery that randomize the image order

--- src/components/galleries/hero/index.tsx
+++ src/components/galleries/hero/index.tsx
@@ -1,6 +1,7 @@
 import { useCallback, useEffect, useMemo, useState } from 'react'
 import type { getImagesFromResponse } from '@/utils/formatter'
-import { AnimatePresence, motion } from 'framer-motion'
+import { AnimatePresence, motion, useAnimationControls } from 'framer-motion'
+import deepClone from '@/utils/deep-clone'
 
 interface GalleryProps {
   images: ReturnType<typeof getImagesFromResponse>
@@ -9,11 +10,49 @@ interface GalleryProps {
 
 const IMAGE_GALLERY_ID_PREFIX = 'image-gallery'
 
+const containerVariants = {
+  initial: {
+    transition: {
+      staggerChildren: 0.1
+    }
+  },
+  animate: {
+    transition: {
+      staggerChildren: 0.1
+    }
+  }
+}
+
+const imageVariants = {
+  initial: {
+    opacity: 0,
+    scale: 0.8,
+    transition: {
+      duration: 0.5
+    }
+  },
+  animate: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration: 0.5
+    }
+  },
+  exit: {
+    opacity: 0,
+    scale: 0.8,
+    transition: {
+      duration: 0.3
+    }
+  }
+}
+
 export default function Gallery({ images, retrieveError }: GalleryProps) {
-  const displayImages = useMemo(() => images?.slice(0, 9) ?? [], [images])
+  const [displayImages, setDisplayImages] = useState(images?.slice(0, 9) ?? [])
   const [loadedImagesIndexes, setLoadedImagesIndexes] = useState<Set<number>>(
     new Set()
   )
+  const galleryAnimationControl = useAnimationControls()
 
   const handleImageLoad = useCallback((imageIndex: number) => {
     setLoadedImagesIndexes((prev) => new Set(prev).add(imageIndex))
@@ -27,10 +66,6 @@ export default function Gallery({ images, retrieveError }: GalleryProps) {
     }
   }, [retrieveError])
 
-  /**
-   * Se asigna el source y el onload programáticamente porque el onload
-   * no se ejecuta cuando las imágenes ya están cacheadas (Ej: soft reload de la página)
-   */
   useEffect(() => {
     const matchElements = document.querySelectorAll(
       `[id^='${IMAGE_GALLERY_ID_PREFIX}-']`
@@ -65,21 +100,55 @@ export default function Gallery({ images, retrieveError }: GalleryProps) {
     }
   }, [displayImages, handleImageLoad])
 
+  useEffect(() => {
+    if (displayImages.length === 0) {
+      return
+    }
+
+    const newImagesArray = deepClone(displayImages)
+
+    const randomizeImages = async () => {
+      await galleryAnimationControl.start('exit')
+
+      let currentIndex = newImagesArray.length
+      while (currentIndex !== 0) {
+        const randomIndex = Math.floor(Math.random() * currentIndex)
+        currentIndex--
+        ;[newImagesArray[currentIndex], newImagesArray[randomIndex]] = [
+          newImagesArray[randomIndex],
+          newImagesArray[currentIndex]
+        ]
+      }
+
+      setDisplayImages(newImagesArray)
+      await galleryAnimationControl.start('animate')
+    }
+
+    const intervalId = setInterval(randomizeImages, 10_000)
+
+    galleryAnimationControl.start('animate')
+
+    return () => clearInterval(intervalId)
+  }, [displayImages, galleryAnimationControl])
+
   return (
-    <div className="relative grid grid-cols-2 md:grid-cols-4 gap-3 pt-12 md:pl-10">
+    <motion.div
+      className="relative grid grid-cols-2 md:grid-cols-4 gap-3 pt-12 md:pl-10"
+      variants={containerVariants}
+      initial="initial"
+      animate={galleryAnimationControl}
+    >
       <AnimatePresence>
         {displayImages.map(({ assetId }, index) => (
           <motion.div
             key={assetId}
             className={`relative rounded-lg overflow-hidden max-h-[100px]
-            ${index === 0 ? 'col-span-2 row-span-2 md:col-span-1 md:row-span-1' : ''}
-            ${index === 1 ? 'col-span-2' : ''}
-            ${index === 4 || index === 5 ? 'row-span-2' : ''}
-            ${index === 6 ? 'col-span-2' : ''}
-          `}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 0.5, delay: index * 0.1 }}
+              ${index === 0 ? 'col-span-2 row-span-2 md:col-span-1 md:row-span-1' : ''}
+              ${index === 1 ? 'col-span-2' : ''}
+              ${index === 4 || index === 5 ? 'row-span-2' : ''}
+              ${index === 6 ? 'col-span-2' : ''}
+            `}
+            variants={imageVariants}
           >
             <motion.div
               className="absolute inset-0 bg-gray-200"
@@ -98,6 +167,6 @@ export default function Gallery({ images, retrieveError }: GalleryProps) {
           </motion.div>
         ))}
       </AnimatePresence>
-    </div>
+    </motion.div>
   )
 }

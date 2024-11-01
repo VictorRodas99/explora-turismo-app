4b7b0a3d92f55079606cbcba33de02ccf525d378
VictorRodas99 <victormatiasrodas100@gmail.com>
2024-11-01 11:59:55 -0300

    adjust the supabase client to accept login and register with oauth

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,5 +3,10 @@ import type { Database } from './database'
 
 export const supabase = createClient<Database>(
   import.meta.env.SUPABASE_URL,
-  import.meta.env.SUPABASE_ANON_KEY
+  import.meta.env.SUPABASE_ANON_KEY,
+  {
+    auth: {
+      flowType: 'pkce'
+    }
+  }
 )

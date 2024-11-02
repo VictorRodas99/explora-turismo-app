b04c59fdd82a8da61ee522732876535b2911aad7
VictorRodas99 <victormatiasrodas100@gmail.com>
2024-11-02 17:23:55 -0300

    refactor code

--- src/types.ts
+++ src/types.ts
@@ -1,25 +1,20 @@
-import type { Database } from './lib/database'
+import type { Tables } from './lib/database'
 import type { interestPointCategories } from './constants'
 
 export type UserRole = 'admin' | 'editor' | 'viewer'
 
-export type Distrito = Database['public']['Tables']['distrito']['Row']
+export type Event = Tables<'eventos'>
+export type Distrito = Tables<'distrito'>
+export type InterestPointDB = Tables<'puntos_de_interes'>
+export type InterestPointImage = Tables<'puntos_de_interes_imagenes'>
+export type InterestPointContactsDB = Tables<'puntos_de_interes_contactos'>
 
 export type InterestPointCategoryEnglish = keyof typeof interestPointCategories
 export type InterestPointCategory =
   (typeof interestPointCategories)[InterestPointCategoryEnglish]
 
-export type InterestPointDB =
-  Database['public']['Tables']['puntos_de_interes']['Row']
-
 export interface InterestPoint extends InterestPointDB {
   tipo: InterestPointCategory
 }
 
-export type Event = Database['public']['Tables']['eventos']['Row']
-export type InterestPointImage =
-  Database['public']['Tables']['puntos_de_interes_imagenes']['Row']
-
-export type InterestPointContactsDB =
-  Database['public']['Tables']['puntos_de_interes_contactos']['Row']
 export type InterestPointContacts = Omit<InterestPointContactsDB, 'created_at'>

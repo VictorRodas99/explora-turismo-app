c26ec8f81f2bdd1266bc6466f6ddb1519bf2b6ab
VictorRodas99 <victormatiasrodas100@gmail.com>
2024-11-02 17:32:01 -0300

    fix persistent session after logout

--- src/pages/api/auth/logout.ts
+++ src/pages/api/auth/logout.ts
@@ -1,6 +1,13 @@
+import { signOut } from '@/services/server/session'
 import type { APIRoute } from 'astro'
 
 export const GET: APIRoute = async ({ cookies, redirect }) => {
+  const { error } = await signOut()
+
+  if (error) {
+    throw error
+  }
+
   cookies.delete('sb-access-token', { path: '/' })
   cookies.delete('sb-refresh-token', { path: '/' })
 

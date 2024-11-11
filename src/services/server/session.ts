1f6adcb837d5521e03a3ae78140852adaa02065a
VictorRodas99 <victormatiasrodas100@gmail.com>
2024-11-11 16:14:48 -0300

    refactor code

--- src/services/server/session.ts
+++ src/services/server/session.ts
@@ -1,19 +1,42 @@
+import { SB_COKIE_SESSION_NAME } from '@/constants'
 import { supabase } from '@/lib/supabase'
+import type { AuthError, Session, User } from '@supabase/supabase-js'
+import type { AstroCookies } from 'astro'
 
-export async function getSession() {
-  const { data, error } = await supabase.auth.getSession()
-
-  if (error) {
-    return {
-      session: null,
-      user: null
+type Args =
+  | {
+      cookies: AstroCookies
     }
+  | undefined
+
+export async function getSession(context: Args = undefined): Promise<{
+  session: Session | null
+  user: User | null
+  error?: AuthError
+}> {
+  const { cookies } = context ?? { cookies: null }
+
+  const supabaseAccessToken = cookies?.get(SB_COKIE_SESSION_NAME.access)
+  const supabaseRefreshToken = cookies?.get(SB_COKIE_SESSION_NAME.refresh)
+
+  if (supabaseAccessToken && supabaseRefreshToken) {
+    const { data, error } = await supabase.auth.setSession({
+      access_token: supabaseAccessToken.value,
+      refresh_token: supabaseRefreshToken.value
+    })
+
+    if (error) {
+      cookies?.delete(SB_COKIE_SESSION_NAME.access, { path: '/' })
+      cookies?.delete(SB_COKIE_SESSION_NAME.refresh, { path: '/' })
+
+      return { session: null, user: null, error }
+    }
+
+    return { session: data.session, user: data.user }
   }
 
-  return {
-    session: data.session,
-    user: data.session?.user ?? null
-  }
+  const { data } = await supabase.auth.getSession()
+  return { session: data.session, user: data?.session?.user ?? null }
 }
 
 export async function signOut() {

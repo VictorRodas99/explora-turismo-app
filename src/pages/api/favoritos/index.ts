a604042221ce63b1a62506e0145f563d0e9584fe
VictorRodas99 <victormatiasrodas100@gmail.com>
2024-11-11 16:20:17 -0300

    update endpoint to get favorite places

--- src/pages/api/favoritos/index.ts
+++ src/pages/api/favoritos/index.ts
@@ -6,6 +6,142 @@ import {
 import { StatusCodes } from 'http-status-codes'
 import { supabase } from '@/lib/supabase'
 import { getTableAttrBasedOn } from './_utils/general'
+import { getSession } from '@/services/server/session'
+import { DEFAULT_API_LIMIT_FOR } from '../_states'
+import { getPagination } from '@/utils/pagination'
+
+export const GET: APIRoute = async ({ cookies, url }) => {
+  const page = Number(url.searchParams.get('page')) ?? 1
+
+  const limitFromParam = Number(url.searchParams.get('limit'))
+  const limit =
+    Number.isNaN(limitFromParam) || limitFromParam <= 0
+      ? DEFAULT_API_LIMIT_FOR.distritos
+      : limitFromParam
+
+  if (Number.isNaN(page)) {
+    return createErrorResponse({
+      errorMessage: 'Page param must be a valid number',
+      responseStatus: StatusCodes.BAD_REQUEST
+    })
+  }
+
+  const { from, to } = getPagination({ page, size: limit })
+
+  try {
+    const { session, error } = await getSession({ cookies })
+
+    if (!session || error) {
+      return createErrorResponse({
+        errorMessage: 'Operación no autorizada',
+        responseStatus: StatusCodes.UNAUTHORIZED
+      })
+    }
+
+    const {
+      data: { user },
+      error: userRetrieveError
+    } = await supabase.auth.getUser(session.access_token)
+
+    if (userRetrieveError || !user) {
+      return createErrorResponse({
+        errorMessage: 'Non valid user',
+        responseStatus: StatusCodes.UNAUTHORIZED
+      })
+    }
+
+    const { data: favoriteDistricts, error: retrieveFavoritesDistrictsError } =
+      await supabase
+        .from('distrito_favoritos')
+        .select(
+          `
+        distrito_id,
+        distrito (
+          id,
+          name,
+          description,
+          main_image,
+          coords
+        )
+      `,
+          { count: 'exact' }
+        )
+        .match({ user_id: user.id })
+        .range(from, to)
+
+    if (retrieveFavoritesDistrictsError) {
+      throw retrieveFavoritesDistrictsError
+    }
+
+    const { data: favoritePoints, error: retrieveFavoritesPointsError } =
+      await supabase
+        .from('punto_interes_favoritos')
+        .select(
+          `
+          punto_de_interes_id,
+          puntos_de_interes (
+            id,
+            name,
+            description,
+            coords,
+            distrito_id,
+            distrito (
+              name
+            )
+          )  
+        `,
+          { count: 'exact' }
+        )
+        .match({ user_id: user.id })
+        .range(from, to)
+
+    if (retrieveFavoritesPointsError) {
+      throw retrieveFavoritesPointsError
+    }
+
+    const getMappedFavoritePoints = (favorites: typeof favoritePoints) => {
+      const mappedFavorites = []
+
+      for (const point of favorites) {
+        const { punto_de_interes_id, puntos_de_interes: data } = point
+
+        if (!data) {
+          continue
+        }
+
+        const { distrito, ...relevantInfo } = data
+
+        mappedFavorites.push({
+          punto_de_interes_id,
+          puntos_de_interes: {
+            ...relevantInfo,
+            distrito_name: distrito?.name ?? null
+          }
+        })
+      }
+
+      return mappedFavorites.length > 0 ? mappedFavorites : null
+    }
+
+    return createSucessResponse({
+      responseStatus: StatusCodes.OK,
+      data: {
+        favoriteDistricts: favoriteDistricts ?? null,
+        favoritePoints: getMappedFavoritePoints(favoritePoints)
+      }
+    })
+  } catch (error) {
+    console.error(error)
+
+    return createErrorResponse({
+      responseStatus: StatusCodes.INTERNAL_SERVER_ERROR,
+      errorMessage:
+        error instanceof Error && 'message' in error
+          ? String(error.message)
+          : 'Unkown server error'
+    })
+  }
+}
 
 export const POST: APIRoute = async ({ request }) => {
   try {
